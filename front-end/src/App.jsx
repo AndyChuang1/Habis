e5e2c59c7819ba1257501b40c6d843b38da8a78c
Andychuang <35476023+AndyChuang1@users.noreply.github.com>
2020-04-11 22:59:58 +0800

    add login example

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,9 +1,13 @@
 import React, { useState } from 'react';
+// import { spacing } from '@material-ui/system';
 import './App.scss';
 import { Route, Switch } from 'react-router-dom';
 import Login from './views/Login';
 import PrivateRoute from './route/PrivateRoute';
 import Home from './views/Home';
+import Search from './views/Search';
+import Bill from './views/Bill';
+import Profile from './views/Profile';
 
 function App() {
   const [token, setToken] = useState(false);
@@ -11,10 +15,19 @@ function App() {
     <div className='App'>
       <Switch>
         <Route path='/login'>
-          <Login></Login>
+          <Login setToken={setToken}></Login>
         </Route>
         <PrivateRoute exact path='/' token={token}>
           <Home></Home>
+        </PrivateRoute>
+        <PrivateRoute path='/search' token={token}>
+          <Search></Search>
+        </PrivateRoute>
+        <PrivateRoute path='/bill' token={token}>
+          <Bill></Bill>
+        </PrivateRoute>
+        <PrivateRoute path='/profile' token={token}>
+          <Profile></Profile>
         </PrivateRoute>
       </Switch>
     </div>
--- front-end/src/views/Login.jsx
+++ front-end/src/views/Login.jsx
@@ -1,9 +1,54 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import { useState } from 'react';
+import Box from '@material-ui/core/Box';
+import { Button } from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
 
-function Login() {
+function Login({ setToken }) {
+  const [account, setAccount] = useState('');
+  const [pwd, setPwd] = useState('');
+  const history = useHistory();
+
+  const handleSubmit = evt => {
+    evt.preventDefault();
+    if ((account === 'admin') & (pwd === 'admin')) {
+      setToken(true);
+      history.push('/');
+    } else {
+      alert('Login failed, Please check account or password!!');
+    }
+  };
+
   return (
     <div id='login'>
       <h1>Login page</h1>
+      <form noValidate autoComplete='off' onSubmit={handleSubmit}>
+        <Box className='account'>
+          <TextField
+            id='outlined-search'
+            label='Account'
+            type='text'
+            variant='outlined'
+            value={account}
+            onChange={e => setAccount(e.target.value)}
+          />
+        </Box>
+        <Box className='password' mt={2} mb={2}>
+          <TextField
+            id='outlined-password-input'
+            label='Password'
+            type='password'
+            autoComplete='current-password'
+            variant='outlined'
+            value={pwd}
+            onChange={e => setPwd(e.target.value)}
+          />
+        </Box>
+        <Button variant='outlined' type='submit'>
+          Login
+        </Button>
+      </form>
     </div>
   );
 }
