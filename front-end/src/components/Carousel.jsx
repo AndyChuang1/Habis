a3a4a26c9ea05df9e0df12a090ea7e282a45f445
Andychuang <35476023+AndyChuang1@users.noreply.github.com>
2020-04-19 16:28:28 +0800

    adjust Ad_Carousel style

--- front-end/src/components/Carousel.jsx
+++ front-end/src/components/Carousel.jsx
@@ -1,10 +1,44 @@
 import Carousel from 'react-material-ui-carousel';
-
+import '../assets/style/Carousel.scss';
 import React from 'react';
-import Paper from '@material-ui/core';
+import { Paper } from '@material-ui/core';
+import resturent1 from '../assets/img/12a-azure-2019-1024x576.jpeg';
+import resturent2 from '../assets/img/the-marine-room-restaurant-in-la-jolla-top11.jpg';
 
 function Ad_Carousel() {
-  return <div>Carousel</div>;
+  var items = [
+    {
+      name: 'Random Name #1',
+      description: 'Probably the most random thing you have ever seen!',
+      image: resturent1,
+    },
+    {
+      name: 'Random Name #2',
+      description: 'Hello World!',
+      image: resturent2,
+    },
+  ];
+
+  return (
+    <div id='Ad_Carousel'>
+      <Carousel autoPlay={false}>
+        {items.map(item => {
+          return <Item item={item} />;
+        })}
+      </Carousel>
+    </div>
+  );
+}
+function Item(props) {
+  return (
+    <Paper>
+      <img src={props.item.image} width='414' height='193'></img>
+      <div className='content'>
+        <h2 className='title'>{props.item.name}</h2>
+        <p className='text'>{props.item.description}</p>
+      </div>
+    </Paper>
+  );
 }
 
 export default Ad_Carousel;
