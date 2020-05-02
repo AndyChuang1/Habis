fda661963d3048ab8489376120f8ce777bc9691c
Andychuang <35476023+AndyChuang1@users.noreply.github.com>
2020-05-02 22:02:32 +0800

    add container ,KEY for map Item

--- front-end/src/components/FoodCard.jsx
+++ front-end/src/components/FoodCard.jsx
@@ -12,95 +12,95 @@ import truck from '../assets/img/truck.jpg';
 import rice from '../assets/img/rice.jpg';
 import Grid from '@material-ui/core/Grid';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
   },
   card: {
     maxWidth: 345,
-    margin: 'auto'
+    margin: 'auto',
   },
 }));
 
 function Ad_Card() {
-    var items = [
-      {
-        id: 'card1',
-        name: 'pizza',
-        description: 'This cheesecake is really yummy. I’m going for another slice.',
-        image: pizza,
-        alt:'pizza pic',
-        title:'Delicious!',
-      },
-      {
-        id: 'card2',
-        name: 'juice',
-        description: 'This cheesecake is really yummy. I’m going for another slice.',
-        image: juice,
-        alt:'juice pic',
-        title:'Delicious!',
-      },
-      {
-        id: 'card3',
-        name: 'donot',
-        description: 'This cheesecake is really yummy. I’m going for another slice.',
-        image: donot,
-        alt:'donot pic',
-        title:'Delicious!',
-      },
-      {
-        id: 'card4',
-        name: 'truck',
-        description: 'This cheesecake is really yummy. I’m going for another slice.',
-        image: truck,
-        alt:'truck pic',
-        title:'Delicious!',
-      },
-      {
-        id: 'card5',
-        name: 'rice',
-        description: 'This cheesecake is really yummy. I’m going for another slice.',
-        image: rice,
-        alt:'rice pic',
-        title:'Delicious!',
-      },
-    ];
-    const classes = useStyles();
-    return (
-      <div id='Ad_Card' className={classes.root}>
-        <Grid container spacing={3}>
-          {items.map(item => {
-            return <Item item={item} />;
-          })}
-        </Grid>
-      </div>
-    );
+  var items = [
+    {
+      id: 'card1',
+      name: 'pizza',
+      description: 'This cheesecake is really yummy. I’m going for another slice.',
+      image: pizza,
+      alt: 'pizza pic',
+      title: 'Delicious!',
+    },
+    {
+      id: 'card2',
+      name: 'juice',
+      description: 'This cheesecake is really yummy. I’m going for another slice.',
+      image: juice,
+      alt: 'juice pic',
+      title: 'Delicious!',
+    },
+    {
+      id: 'card3',
+      name: 'donot',
+      description: 'This cheesecake is really yummy. I’m going for another slice.',
+      image: donot,
+      alt: 'donot pic',
+      title: 'Delicious!',
+    },
+    {
+      id: 'card4',
+      name: 'truck',
+      description: 'This cheesecake is really yummy. I’m going for another slice.',
+      image: truck,
+      alt: 'truck pic',
+      title: 'Delicious!',
+    },
+    {
+      id: 'card5',
+      name: 'rice',
+      description: 'This cheesecake is really yummy. I’m going for another slice.',
+      image: rice,
+      alt: 'rice pic',
+      title: 'Delicious!',
+    },
+  ];
+  const classes = useStyles();
+  return (
+    <div id='Ad_Card' className={classes.root} justify='center'>
+      <Grid container spacing={3}>
+        {items.map((item, index) => {
+          return <Item item={item} key={index} />;
+        })}
+      </Grid>
+    </div>
+  );
 }
 
 function Item(props) {
-    const classes = useStyles();
-    return (
-        <Grid item xs={12} sm={6} md={4} lg={3} xl={2}>
-            <Card className={classes.card}>
-                <CardActionArea>
-                    <CardMedia
-                        component="img"
-                        alt= {props.item.alt}
-                        height="140"
-                        image = {props.item.image}
-                        title= {props.item.title}
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="h2">
-                            {props.item.name}
-                        </Typography>
-                        <Typography variant="body2" color="textSecondary" component="p">
-                            {props.item.description}
-                        </Typography>
-                    </CardContent>
-                </CardActionArea>      
-            </Card>
-        </Grid>
-    );
+  const classes = useStyles();
+  return (
+    <Grid item xs={12} sm={6} md={4} lg={3} xl={2}>
+      <Card className={classes.card}>
+        <CardActionArea>
+          <CardMedia
+            component='img'
+            alt={props.item.alt}
+            height='140'
+            image={props.item.image}
+            title={props.item.title}
+          />
+          <CardContent>
+            <Typography gutterBottom variant='h5' component='h2'>
+              {props.item.name}
+            </Typography>
+            <Typography variant='body2' color='textSecondary' component='p'>
+              {props.item.description}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </Grid>
+  );
 }
 export default Ad_Card;
