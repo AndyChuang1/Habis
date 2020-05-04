897f257497326d06eac2aaba225ec67fe9156461
Andychuang <35476023+AndyChuang1@users.noreply.github.com>
2020-05-04 22:04:35 +0800

    remove slick ,add porp to foodcard components

--- front-end/src/components/multiCard.jsx
+++ front-end/src/components/multiCard.jsx
@@ -7,14 +7,10 @@ import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
-import pizza from '../assets/img/pizza.jpg';
-import juice from '../assets/img/juice.jpg';
-import donot from '../assets/img/donot.jpg';
-import truck from '../assets/img/truck.jpg';
-import rice from '../assets/img/rice.jpg';
+
 import Grid from '@material-ui/core/Grid';
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
@@ -27,63 +23,21 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-function Ad_testCard() {
-  var items = [
-    {
-      id: 'card1',
-      name: 'pizza',
-      description: 'This cheesecake is really yummy. I’m going for another slice.',
-      image: pizza,
-      alt: 'pizza pic',
-      title: 'Delicious!',
-    },
-    {
-      id: 'card2',
-      name: 'juice',
-      description: 'This cheesecake is really yummy. I’m going for another slice.',
-      image: juice,
-      alt: 'juice pic',
-      title: 'Delicious!',
-    },
-    {
-      id: 'card3',
-      name: 'donot',
-      description: 'This cheesecake is really yummy. I’m going for another slice.',
-      image: donot,
-      alt: 'donot pic',
-      title: 'Delicious!',
-    },
-    {
-      id: 'card4',
-      name: 'truck',
-      description: 'This cheesecake is really yummy. I’m going for another slice.',
-      image: truck,
-      alt: 'truck pic',
-      title: 'Delicious!',
-    },
-    {
-      id: 'card5',
-      name: 'rice',
-      description: 'This cheesecake is really yummy. I’m going for another slice.',
-      image: rice,
-      alt: 'rice pic',
-      title: 'Delicious!',
-    },
-  ];
+function MultiCard({ storeDatas }) {
   const classes = useStyles();
   return (
-    <div id='Ad_testCard' className={classes.root}>
+    <div id="Ad_testCard" className={classes.root}>
       <Carousel
         additionalTransfrom={0}
         autoPlaySpeed={3000}
         centerMode={false}
         // removeArrowOnDeviceType={['mobile', 'tablet']}
-        className=''
-        containerClass='container-with-dots'
-        dotListClass=''
+        className=""
+        containerClass="container-with-dots"
+        dotListClass=""
         focusOnSelect={false}
         infinite
-        itemClass=''
+        itemClass=""
         keyBoardControl
         minimumTouchDrag={80}
         renderButtonGroupOutside={false}
@@ -115,11 +69,11 @@ function Ad_testCard() {
           },
         }}
         howDots={true}
-        sliderClass=''
+        sliderClass=""
         slidesToSlide={1}
         swipeable
       >
-        {items.map((item, index) => {
+        {storeDatas.map((item, index) => {
           return <Item item={item} key={index} />;
         })}
       </Carousel>
@@ -134,17 +88,17 @@ function Item(props) {
       <Card className={classes.card}>
         <CardActionArea>
           <CardMedia
-            component='img'
+            component="img"
             alt={props.item.alt}
-            height='140'
+            height="140"
             image={props.item.image}
             title={props.item.title}
           />
           <CardContent>
-            <Typography gutterBottom variant='h5' component='h2'>
+            <Typography gutterBottom variant="h5" component="h2">
               {props.item.name}
             </Typography>
-            <Typography variant='body2' color='textSecondary' component='p'>
+            <Typography variant="body2" color="textSecondary" component="p">
               {props.item.description}
             </Typography>
           </CardContent>
@@ -153,4 +107,4 @@ function Item(props) {
     </Grid>
   );
 }
-export default Ad_testCard;
+export default MultiCard;
--- front-end/src/views/Home.jsx
+++ front-end/src/views/Home.jsx
@@ -1,13 +1,61 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import BottomNav from '../components/BottomNav';
 import Carousel from '../components/Carousel';
 import FoodCard from '../components/FoodCard';
-import CarouselFoodCard from '../components/CarouselFoodCard';
-import Card from '../components/multiCard';
+import Card from '../components/MultiCard';
+import MultiCard from '../components/MultiCard';
+import pizza from '../assets/img/pizza.jpg';
+import juice from '../assets/img/juice.jpg';
+import donot from '../assets/img/donot.jpg';
+import truck from '../assets/img/truck.jpg';
+import rice from '../assets/img/rice.jpg';
+var items = [
+  {
+    id: 'card1',
+    name: 'pizza',
+    description: 'This cheesecake is really yummy. I’m going for another slice.',
+    image: pizza,
+    alt: 'pizza pic',
+    title: 'Delicious!',
+  },
+  {
+    id: 'card2',
+    name: 'juice',
+    description: 'This cheesecake is really yummy. I’m going for another slice.',
+    image: juice,
+    alt: 'juice pic',
+    title: 'Delicious!',
+  },
+  {
+    id: 'card3',
+    name: 'donot',
+    description: 'This cheesecake is really yummy. I’m going for another slice.',
+    image: donot,
+    alt: 'donot pic',
+    title: 'Delicious!',
+  },
+  {
+    id: 'card4',
+    name: 'truck',
+    description: 'This cheesecake is really yummy. I’m going for another slice.',
+    image: truck,
+    alt: 'truck pic',
+    title: 'Delicious!',
+  },
+  {
+    id: 'card5',
+    name: 'rice',
+    description: 'This cheesecake is really yummy. I’m going for another slice.',
+    image: rice,
+    alt: 'rice pic',
+    title: 'Delicious!',
+  },
+];
 function Home() {
+  const [storeItems, setStoreItems] = useState(items);
   return (
-    <div id='home'>
+    <div id="home">
       <h1>Home page</h1>
       {/* <nav>
         <ul>
@@ -27,9 +75,7 @@ function Home() {
       </nav> */}
       <Carousel></Carousel>
       {/* <BottomNav></BottomNav> */}
-      <CarouselFoodCard></CarouselFoodCard> 
-      <Card></Card>
-      <FoodCard></FoodCard>
+      <MultiCard storeDatas={storeItems}></MultiCard>
     </div>
   );
 }
