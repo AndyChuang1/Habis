755dd7621ec6e070c35db892a5fa6e37681728ce
LayJay <rogerchan1226@gmail.com>
2020-05-06 14:04:35 +0800

    [back-end] Update several db fileds & optimize code formate.

--- back-end/modules/DBhelper.js
+++ back-end/modules/DBhelper.js
@@ -10,28 +10,84 @@ module.exports = {
   dbInit() {
     db.serialize(function () {
       //db.run 如果 Staff 資料表不存在，那就建立 Staff 資料表
-      //User 關聯的ＴＡＢＬＥ
+      //User 關聯的 Table
       db.run(
-        'CREATE TABLE IF NOT EXISTS Profile (profileId INTEGER  NOT NULL PRIMARY KEY AUTOINCREMENT,name TEXT NOT NULL, address TEXT NOT NULL, email TEXT, phone INTEGER)'
+        'CREATE TABLE IF NOT EXISTS Profile (' +
+          'profileId INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,' +
+          'name      TEXT NOT NULL,' +
+          'address   TEXT NOT NULL,' +
+          'email     TEXT,' +
+          'phone     INTEGER)'
       );
       db.run(
-        'CREATE TABLE IF NOT EXISTS User (userId INTEGER  NOT NULL PRIMARY KEY AUTOINCREMENT,account TEXT NOT NULL, password TEXT NOT NULL, salt TEXT NOT NULL,' +
-          'profileId INTEGER,FOREIGN KEY (profileId) REFERENCES Profile(profileId) )'
+        'CREATE TABLE IF NOT EXISTS User (' +
+          'userId   INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,' + 
+          'account  TEXT NOT NULL,' + 
+          'password TEXT NOT NULL,' + 
+          'salt     TEXT NOT NULL,' +
+            'profileId INTEGER,FOREIGN KEY (profileId) REFERENCES Profile(profileId) )'
       );
       db.run(
-        'CREATE TABLE IF NOT EXISTS Store (storeId INTEGER  NOT NULL PRIMARY KEY AUTOINCREMENT,name TEXT NOT NULL, openTime DATETIME NOT NULL,closeTime DATETIME NOT NULL, address TEXT NOT NULL,' +
-          'longitude REAL NOT NULL ,latitude REAL NOT NULL )'
+        'CREATE TABLE IF NOT EXISTS Store (' + 
+          'storeId   INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,' +
+          'name      TEXT NOT NULL,' +
+          'openTime  DATETIME NOT NULL,' +
+          'closeTime DATETIME NOT NULL,' + 
+          'address   TEXT NOT NULL,' +
+          'longitude REAL NOT NULL,' +
+          'latitude  REAL NOT NULL )'
       );
       db.run(
-        'CREATE TABLE IF NOT EXISTS StoreImage (imgId INTEGER  NOT NULL PRIMARY KEY AUTOINCREMENT,imgPath TEXT NOT NULL, storeId INTEGER NOT NULL,' +
-          'FOREIGN KEY (storeId) REFERENCES Store(storeId) )'
+        'CREATE TABLE IF NOT EXISTS StoreImage (' +
+          'imgId   INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,' +
+          'imgPath TEXT NOT NULL,' + 
+          'storeId INTEGER NOT NULL,' +
+            'FOREIGN KEY (storeId) REFERENCES Store(storeId) )'
       );
       db.run(
-        'CREATE TABLE IF NOT EXISTS Product (productId INTEGER  NOT NULL PRIMARY KEY AUTOINCREMENT,name TEXT NOT NULL, price REAL NOT NULL, discount REAL NOT NULL,expiredTime DATETIME,quantity INTEGER NOT NULL,imgPath TEXT,description TEXT,storeId INTEGER NOT NULL,' +
-          'FOREIGN KEY (storeId) REFERENCES Store(storeId) )'
+        'CREATE TABLE IF NOT EXISTS Product (' +
+          'productId INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,' +
+          'name        TEXT NOT NULL,' +
+          'price       REAL NOT NULL,' +
+          'discount    REAL NOT NULL,' +
+          'expiredTime DATETIME NOT NULL,' +
+          'quantity    INTEGER NOT NULL,' +
+          'imgPath     TEXT,' +
+          'description TEXT,' +
+          'storeId     INTEGER NOT NULL,' +
+            'FOREIGN KEY (storeId) REFERENCES Store(storeId) )'
+      );
+      db.run(
+        'CREATE TABLE IF NOT EXISTS Order (' +
+          'orderId    INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,' +
+          'userId     INTEGER,' +
+          'updateTime DATETIME NOT NULL,' +
+          'note       TEXT,' +
+            'FOREIGN KEY (userId) REFERENCES User(userId) )'
+      );
+      db.run(
+        'CREATE TABLE IF NOT EXISTS Product (' +
+          'productId   INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,' +
+          'name        TEXT NOT NULL,' + 
+          'price       REAL NOT NULL,' +
+          'discount    REAL NOT NULL,' +
+          'expiredTime DATETIME NOT NULL,' +
+          'quantity    INTEGER NOT NULL,' +
+          'imgPath     TEXT NOT NULL,' +
+          'storeID     INTEGER NOT NULL,' +
+          'description TEXT)'
+      );
+      db.run(
+        'CREATE TABLE IF NOT EXISTS OrderDetail (' +
+          'detailId  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,' +
+          'orderId   INTEGER,' +
+          'quantity  INTEGER NOT NULL,' +
+          'productId INTEGER,' +
+            'FOREIGN KEY (orderId)   REFERENCES Order(orderId),' +
+            'FOREIGN KEY (productId) REFERENCES Product(productId) )'
       );
 
-      // Store 關聯的Ｔable
+      // Store 關聯的 Table
       //   db.close();
       console.log('DB Init successfuly');
     });
